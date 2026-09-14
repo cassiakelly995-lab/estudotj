7a969103936b1a3adde853dc58af25b4dd629f90
Cassia Kelly Lins Reis <cassiakelly995@gmail.com>
2026-09-14 12:41:18 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'estudo-tjsp-v1';
+const ARQUIVOS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARQUIVOS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((nomes) =>
+      Promise.all(nomes.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((resp) => {
+        const respClone = resp.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, respClone));
+        return resp;
+      }).catch(() => cached);
+    })
+  );
+});
